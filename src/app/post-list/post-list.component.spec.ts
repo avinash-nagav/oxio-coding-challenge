b07d4fd9eccb88222f1cdf95509f0b288f597b6c
Avinash <kollivelasiavinash@gmail.com>
2024-08-18 20:23:28 -0500

    test: add test case for sorting the user id column in asc/des order

--- src/app/post-list/post-list.component.spec.ts
+++ src/app/post-list/post-list.component.spec.ts
@@ -1,9 +1,15 @@
 /* tslint:disable:no-unused-variable */
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  waitForAsync,
+  ComponentFixture,
+  TestBed,
+} from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { PostListComponent } from './post-list.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
 describe('PostListComponent', () => {
   let component: PostListComponent;
@@ -11,9 +17,10 @@ describe('PostListComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ PostListComponent ]
-    })
-    .compileComponents();
+      declarations: [],
+      imports: [],
+      providers: [provideNoopAnimations(), provideHttpClient()],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -24,5 +31,25 @@ describe('PostListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should sort the User Id column in ascending order', () => {
+    const el = fixture.debugElement;
+    const userIdHeaderEl: DebugElement = el.query(By.css('.mat-column-userId'));
+    userIdHeaderEl.triggerEventHandler('click');
+    fixture.detectChanges();
+    fixture.detectChanges();
+    const sortAttr =  userIdHeaderEl.attributes['aria-sort'];
+    expect(sortAttr).toBe('ascending');
+  });
+
+  it('should sort the User Id column in descending order', () => {
+    const el = fixture.debugElement;
+    const userIdHeaderEl: DebugElement = el.query(By.css('.mat-column-userId'));
+    userIdHeaderEl.triggerEventHandler('click');
+    userIdHeaderEl.triggerEventHandler('click');
+    fixture.detectChanges();
+    const sortAttr = userIdHeaderEl.attributes['aria-sort']
+    expect(sortAttr).toBe('descending');
   });
 });
